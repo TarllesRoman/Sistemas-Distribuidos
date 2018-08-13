7ac255a3edeed01c4646c07ea3955787a66dcf27
Tarlles Roman <roman.tarlles@gmail.com>
2018-08-13 18:29:00 -0300

    Erros corrigidos, segunda atividade realizada

---
+++ Comandos Remotos/cliente2.py
@@ -0,0 +1,79 @@
+#coding: utf-8
+
+from socket import *
+import subprocess
+import commands
+
+server_ip = '127.0.0.1'
+server_port = 19199
+
+serverOFF = False
+
+mensagem = ""
+
+clienteSocket = socket(AF_INET, SOCK_STREAM)
+
+def server_on():
+	global server_ip
+	global server_port
+	global serverOFF
+	global mensagem
+	global clienteSocket
+	
+	try:
+		print "Conectado ao servidor!"
+		while mensagem != '\x18':
+			clienteSocket.send(mensagem.encode("utf-8"))
+				
+			resposta = clienteSocket.recv(2048)
+			if(resposta == ""):
+				server_off()
+			print "Resposta:", resposta
+				
+			mensagem = raw_input("Digite uma mensagem: ")
+	except:
+		clienteSocket.close()
+		clienteSocket = socket(AF_INET, SOCK_STREAM)
+		serverOFF = True
+		server_off()			
+			
+
+def server_off():
+	global server_ip
+	global server_port
+	global serverOFF
+	global mensagem
+	
+	print "Executando localmente"
+	while mensagem != '\x18':
+		print "Resposta:", commands.getoutput(mensagem) 
+		mensagem = raw_input("Digite uma mensagem: ")
+		connect()
+		if(not serverOFF):
+			server_on()
+
+
+def connect():
+	global server_ip
+	global server_port
+	global serverOFF
+	global clienteSocket
+	
+	try:
+		clienteSocket.connect((server_ip,server_port))
+		serverOFF = False
+	except Exception as e:
+		print e 
+		serverOFF = True
+
+
+connect()
+print "Digite ctrl+X para sair"
+mensagem = raw_input("Digite uma mensagem: ")
+
+if serverOFF:
+	server_off()
+else:
+	server_on()
+
+clienteSocket.close()
--- Comandos Remotos/servidor.py
+++ Comandos Remotos/servidor.py
@@ -10,20 +10,15 @@ serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('', server_port))
 serverSocket.listen(1)
 
-
 while True:
 	print "Server pronto para aceitar conexao"
 	
 	conexao, cliente = serverSocket.accept()
 	print "Conexao aceita"
-
-	while True:		
+	
+	while True:
 		message = conexao.recv(2048)
 		
-		if(message == ""):
-			print "Conexao encerrada"
-			conexao.close()
-			break
 		if(message=="EXIT"):
 			print "Conexao encerrada"
 			conexao.close()
@@ -31,7 +26,6 @@ while True:
 			
 		print "Mensagem recebida: ",message.decode("utf-8")
 		resultado = commands.getoutput(message.decode("utf-8"))
-		
 		conexao.send(resultado)
 	
 serverSocket.close()
