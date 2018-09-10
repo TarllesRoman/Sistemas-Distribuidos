a8ab7e8ab0ee93c066e0d5ca80d808e4fd32717f
Tarlles Roman <40526902+TarllesRoman@users.noreply.github.com>
2018-09-10 16:02:42 -0300

    .

---
+++ Modulos/main.py
@@ -0,0 +1,11 @@
+#coding: utf-8
+
+import modulo
+
+print ("Numeros: 10 e 20")
+
+print  "Soma: ", modulo.soma(10,20)
+print  "Subtracao: ", modulo.subtracao(10,20)
+print  "Divisao: ", modulo.divisao(10,20)
+print  "Multiplicacao: ", modulo.multiplicacao(10,20)
+
